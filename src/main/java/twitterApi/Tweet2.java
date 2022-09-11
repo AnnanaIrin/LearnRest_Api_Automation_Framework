e920fb8468a8366ad765dd46eccf89ce7107adcc
AnnanaIrin <msakther786@gmail.com>
2022-09-11 10:57:51 -0400

    twitter api version 1.1 done

---
+++ src/main/java/twitterApi/Tweet2.java
@@ -0,0 +1,31 @@
+package twitterApi;
+
+import base.RestBase;
+
+public class Tweet2  extends RestBase {
+
+    //https://developer.twitter.com/en/docs/twitter-api/tweets/timelines/api-reference/get-users-id-tweets
+    //End point:
+    //https://api.twitter.com/2/users/:id/tweets
+
+    public final String SINGLE_USER_TWEET_ENDPOINT="users/:id/tweets";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
